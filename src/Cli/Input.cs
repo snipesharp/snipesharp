77d5dbf52b736fcb43e5830a884c02c9b4deb6f7
Stiliyan Kushev <stiliyankushev@gmail.com>
2022-02-12 12:35:25 +0200

    moved arguments logic to seperate Core class of Cli

--- src/Cli/Input.cs
+++ src/Cli/Input.cs
@@ -1,20 +1,5 @@
 namespace Cli {
     class Input {
-        public static Dictionary<string, Argument> arguments = ParseArgs();
-
-        // parse the command line arguments and convert them to a dictionary
-        public static Dictionary<string, Argument> ParseArgs(){
-            List<string> args = Environment.GetCommandLineArgs().Skip(1).ToList();
-            var parsedArguments = new Dictionary<string, Argument>();
-            
-            args.ForEach(arg => {
-                var parsed = new Argument(arg);
-                parsedArguments[parsed.name] = parsed;
-            });
-
-            return parsedArguments;
-        }
-
         // small method for requesting input from the user
         public static T Request<T>(string requestMessage){
             while(true){
