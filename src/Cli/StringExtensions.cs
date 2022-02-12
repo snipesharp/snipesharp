bbc206043708636f3ae041ebd6e816b628ccd4dd
Stiliyan Kushev <stiliyankushev@gmail.com>
2022-02-12 12:27:05 +0200

    Added string extention method for centering

--- src/Cli/StringExtensions.cs
+++ src/Cli/StringExtensions.cs
@@ -2,36 +2,46 @@ namespace StringExtensions
 {
     public static class StringExtension
     {
-        public static string Black(this string str) {
+        /* Add basic color support */
+        public static string Black(this string str){
             return $"\u001b[30m{str}\u001b[0m";
         }
 
-        public static string Red(this string str) {
+        public static string Red(this string str){
             return $"\u001b[31m{str}\u001b[0m";
         }
 
-        public static string Green(this string str) {
+        public static string Green(this string str){
             return $"\u001b[32m{str}\u001b[0m";
         }
 
-        public static string Yellow(this string str) {
+        public static string Yellow(this string str){
             return $"\u001b[33m{str}\u001b[0m";
         }
 
-        public static string Blue(this string str) {
+        public static string Blue(this string str){
             return $"\u001b[34m{str}\u001b[0m";
         }
 
-        public static string Magenta(this string str) {
+        public static string Magenta(this string str){
             return $"\u001b[35m{str}\u001b[0m";
         }
 
-        public static string Cyan(this string str) {
+        public static string Cyan(this string str){
             return $"\u001b[36m{str}\u001b[0m";
         }
 
-        public static string White(this string str) {
+        public static string White(this string str){
             return $"\u001b[37m{str}\u001b[0m";
+        }
+
+        /* Add basic responsive printing functionality */
+        public static string Centered(string text){
+            int spacesToAdd = text.Length % 2 != 0
+                ? (Console.WindowWidth - 1) - text.Length 
+                : Console.WindowWidth - text.Length;
+            string spaces = new string(' ', (spacesToAdd / 2));
+            return spaces + text + spaces;
         }
     }
 }
