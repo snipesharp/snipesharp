a4f17446ba167d3b4635517dbe11768697a9fefd
dement6d <dement6d@dement6d.dem>
2022-02-15 18:38:25 +0100

    account for an exception in GetAccount
    previous commit with the same title accounted for an exception in AccountFileExists, not in GetAccount

--- src/FileSystem/FileSystem.cs
+++ src/FileSystem/FileSystem.cs
@@ -9,8 +9,10 @@ namespace FS
             ? Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\.snipesharp\" 
             : Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"/.snipesharp/";
         static string accountJsonFile = snipesharpFolder + "account.json";
-        
-        // Saves the given string to the account.txt file
+
+        /// <summary>
+        /// Saves the given string to the account.txt file
+        /// </summary>
         public static void SaveAccount(Account account){
             try {
                 if (!Directory.Exists(snipesharpFolder)) Directory.CreateDirectory(snipesharpFolder);
@@ -20,11 +22,15 @@ namespace FS
         }
     
         public static Account GetAccount() {
+            if (!AccountFileExists()) return new Account();
             return JsonSerializer.Deserialize<Account>(File.ReadAllText(accountJsonFile));
         }
-
+        /// <summary>
+        /// Checks whether the account.json exists in the snipesharp folder
+        /// </summary>
         /// <returns>true if account.txt exists in the snipesharp folder</returns>
         public static bool AccountFileExists() {
+            if (!Directory.Exists(snipesharpFolder)) return false;
             return File.Exists(accountJsonFile);
         }
     }
