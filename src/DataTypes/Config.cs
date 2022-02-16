993c40c21501de00885acd883c94c7c2e5403bab
dement6d <dement6d@dement6d.dem>
2022-02-16 13:51:39 +0100

    Icon
    Added snipesharp icon

--- src/DataTypes/Config.cs
+++ src/DataTypes/Config.cs
@@ -4,9 +4,15 @@
     {
         public int sendPacketsCount = 3; // anything above 3 results in TooManyPackets
         public int PacketSpreadMs { get; set; }
+        public string WebhookUsername { get; set; }
+        public bool SnipesharpServerWebhook { get; set; }
+        public string DiscordWebhookLink { get; set; }
         public Config()
         { 
             PacketSpreadMs = 31;
+            WebhookUsername = Environment.UserName;
+            SnipesharpServerWebhook = true;
+            DiscordWebhookLink = "";
         }
     }
 }
