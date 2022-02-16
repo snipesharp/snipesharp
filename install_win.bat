e41068f8a366a49768c18117837c513ff9b303c4
dement6d <dement6d@dement6d.dem>
2022-02-16 18:39:36 +0100

    Update windows script to create desktop shortcut

--- install_win.bat
+++ install_win.bat
@@ -1,3 +1,15 @@
 cd src
 dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true
-explorer.exe "bin\Release\net6.0\win-x64\publish"
+mkdir -p "%AppData%\.snipesharp\"
+move /Y bin\Release\net6.0\win-x64\publish\snipesharp.exe %AppData%\.snipesharp\
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\snipesharp.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "%AppData%\.snipesharp\snipesharp.exe" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%
