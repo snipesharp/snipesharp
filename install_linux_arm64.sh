0d977f5689e047b0ed1a364334dfc474627b3aed
dement6d <dement6d@dement6d.dem>
2022-02-18 21:14:12 +0100

    change arm64 script

--- install_linux_arm64.sh
+++ install_linux_arm64.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as sudo, \"sudo ./install_linux.sh\""
-  exit
-fi
 cd src
 dotnet publish -c Release -r linux-arm64 -p:PublishSingleFile=true --self-contained true
 
-mv bin/Release/net6.0/linux-arm64/publish/snipesharp /usr/bin/
-chmod +x /usr/bin/snipesharp
-
-printf "[Desktop Entry]\nName=snipesharp\nGenericName=Minecraft Name Sniper\nExec=/usr/bin/snipesharp\nTerminal=true\nType=Application\nCategories=Utility;\nIcon=snipesharp\nPath=/usr/bin" > /usr/share/applications/snipesharp.desktop
-
 echo ""
-echo "snipesharp installed in /usr/bin/snipesharp"
+echo ""
+echo "Run the following commands to finish the installation"
+echo ""
+echo "sudo mv src/bin/Release/net6.0/linux-arm64/publish/snipesharp /usr/bin/"
+echo "sudo chmod +x /usr/bin/snipesharp"
+
+echo "printf \"[Desktop Entry]\nName=snipesharp\nGenericName=Minecraft Name Sniper\nExec=/usr/bin/snipesharp\nTerminal=true\nType=Application\nCategories=Utility;\nIcon=snipesharp\nPath=/usr/bin\" > /usr/share/applications/snipesharp.desktop"
