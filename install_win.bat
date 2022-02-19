2cd9e09347005fd541efe7365e4f7985641cf96e
dement6d <dement6d@dement6d.dem>
2022-02-19 16:02:25 +0100

    improve windows install script

--- install_win.bat
+++ install_win.bat
@@ -1,8 +1,17 @@
+@echo off
 cd src
-dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true
-mkdir "%AppData%\.snipesharp\"
-move /Y bin\Release\net6.0\win-x64\publish\snipesharp.exe %AppData%\.snipesharp\
 
+echo Compiling...
+@echo.
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained true
+@echo.
+echo Making folder "%AppData%\.snipesharp\"
+mkdir %AppData%\.snipesharp\
+@echo.
+echo Moving snipesharp.exe to "%AppData%\.snipesharp\"
+move /Y bin\Release\net6.0\win-x64\publish\snipesharp.exe %AppData%\.snipesharp\
+@echo.
+echo Creating desktop shortcut for snipesharp.exe
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
@@ -14,3 +23,5 @@ echo oLink.Save >> %SCRIPT%
 
 cscript /nologo %SCRIPT%
 del %SCRIPT%
+@echo.
+pause
