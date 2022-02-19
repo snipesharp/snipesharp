44dea2856977119031b4fd09a15acebc968b1583
dement6d <dement6d@dement6d.dem>
2022-02-19 01:31:13 +0100

    update linux arm64 script

--- install_linux_arm64.sh
+++ install_linux_arm64.sh
@@ -2,12 +2,7 @@
 
 cd src
 dotnet publish -c Release -r linux-arm64 -p:PublishSingleFile=true --self-contained true
+sudo mv bin/Release/net6.0/linux-arm64/publish/snipesharp /usr/bin/
+sudo chmod +x /usr/bin/snipesharp
 
-echo ""
-echo ""
-echo "Run the following commands to finish the installation"
-echo ""
-echo "sudo mv src/bin/Release/net6.0/linux-arm64/publish/snipesharp /usr/bin/"
-echo "sudo chmod +x /usr/bin/snipesharp"
-
-echo "printf \"[Desktop Entry]\nName=snipesharp\nGenericName=Minecraft Name Sniper\nExec=/usr/bin/snipesharp\nTerminal=true\nType=Application\nCategories=Utility;\nIcon=snipesharp\nPath=/usr/bin\" > /usr/share/applications/snipesharp.desktop"
+printf "[Desktop Entry]\nName=snipesharp\nGenericName=Minecraft Name Sniper\nExec=/usr/bin/snipesharp\nTerminal=true\nType=Application\nCategories=Utility;\nIcon=snipesharp\nPath=/usr/bin" > /usr/share/applications/snipesharp.desktop"
