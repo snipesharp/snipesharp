4e1cec9dfe270f038128020f903baea3849deac6
dement6d <cuff.ho@gmail.com>
2022-03-07 00:45:51 +0100

    update install linux script

--- install_linux.sh
+++ install_linux.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as sudo, \"sudo ./install_linux.sh\""
-  exit
-fi
 cd src
 dotnet publish -c Release -r linux-x64 -p:PublishSingleFile=true --self-contained true
 
-mv bin/Release/net6.0/linux-x64/publish/snipesharp /usr/bin/
-chmod +x /usr/bin/snipesharp
+sudo mv bin/Release/net6.0/linux-x64/publish/snipesharp /usr/bin/
+sudo chmod +x /usr/bin/snipesharp
 
-printf "[Desktop Entry]\nName=snipesharp\nGenericName=Minecraft Name Sniper\nExec=/usr/bin/snipesharp\nTerminal=true\nType=Application\nCategories=Utility;\nIcon=snipesharp\nPath=/usr/bin" > /usr/share/applications/snipesharp.desktop
+sudo bash -c "echo $'[Desktop Entry]\nName=snipesharp\nGenericName=Minecraft Name Sniper\nExec=/usr/bin/snipesharp\nTerminal=true\nType=Application\nCategories=Utility;\nIcon=snipesharp\nPath=/usr/bin' > /usr/share/applications/snipesharp.desktop"
 
 echo ""
 echo "snipesharp installed in /usr/bin/snipesharp"
