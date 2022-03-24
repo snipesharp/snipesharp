613ea3fd312fdf7e4b4b465bf91f25fd189f765f
dement6d <cuff.ho@gmail.com>
2022-03-24 19:12:40 +0100

    fix oopsie

--- src/Snipe/Sniper.cs
+++ src/Snipe/Sniper.cs
@@ -27,7 +27,7 @@ namespace Snipe
 
             // actually wait for the time
             int msToSleep = (int)TimeSpan.FromMilliseconds(droptime).TotalMilliseconds;
-            Thread.Sleep(msToSleep <= 0 ? msToSleep : 0 - DateTime.Now.Millisecond); // take away the current milliseconds
+            Thread.Sleep(msToSleep <= 0 ? 0 : msToSleep - DateTime.Now.Millisecond); // take away the current milliseconds
 
             countDown.Cancel();
         }
