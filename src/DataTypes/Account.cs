23922f8c7c1edf6d491878de26e7fb5e0da3efca
dement6d <cuff.ho@gmail.com>
2022-04-27 19:03:53 +0200

    remove security questions from account.cs since theyre not used

--- src/DataTypes/Account.cs
+++ src/DataTypes/Account.cs
@@ -10,12 +10,6 @@ namespace DataTypes
         public string MicrosoftPassword { get; set; }
         public string MojangEmail { get; set; }
         public string MojangPassword { get; set; }
-        [JsonPropertyName("sq1")]
-        public string SecurityQuestion1 { get; set; }
-        [JsonPropertyName("sq2")]
-        public string SecurityQuestion2 { get; set; }
-        [JsonPropertyName("sq3")]
-        public string SecurityQuestion3 { get; set; }
     }
     public static class Account {
         public static AccountSettings v = new AccountSettings();
