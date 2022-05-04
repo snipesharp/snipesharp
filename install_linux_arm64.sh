6adfa5c34c4efac936c101dfa053e263d58eb852
dement6d <cuff.ho@gmail.com>
2022-05-04 08:07:41 +0200

    change arm64 script

--- install_linux_arm64.sh
+++ install_linux_arm64.sh
@@ -2,7 +2,11 @@
 
 cd src
 dotnet publish -c Release -r linux-arm64 -p:PublishSingleFile=true --self-contained true
+
 sudo mv bin/Release/net6.0/linux-arm64/publish/snipesharp /usr/bin/
 sudo chmod +x /usr/bin/snipesharp
 
-sudo printf "[Desktop Entry]\nName=snipesharp\nGenericName=Minecraft Name Sniper\nExec=/usr/bin/snipesharp\nTerminal=true\nType=Application\nCategories=Utility;\nIcon=snipesharp\nPath=/usr/bin" > /usr/share/applications/snipesharp.desktop"
+sudo bash -c "echo $'[Desktop Entry]\nName=snipesharp\nGenericName=Minecraft Name Sniper\nExec=/usr/bin/snipesharp\nTerminal=true\nType=Application\nCategories=Utility;\nIcon=snipesharp\nPath=/usr/bin' > /usr/share/applications/snipesharp.desktop"
+
+echo ""
+echo "snipesharp installed in /usr/bin/snipesharp"
