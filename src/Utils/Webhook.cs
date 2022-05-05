a197581f687e4f1ffb40b52f0277254761a343e8
dement6d <cuff.ho@gmail.com>
2022-05-05 18:15:36 +0200

    fix custom webhooks

--- src/Utils/Webhook.cs
+++ src/Utils/Webhook.cs
@@ -5,17 +5,19 @@ namespace Utils
 {
     public class Webhook
     {
-        public static void Send(string webhookLink, string content) {
+        public static async Task<string> Send(string webhookLink, string content) {
             try {
                 HttpClient client = new HttpClient();
                 StringContent stringContent = new StringContent(content, System.Text.Encoding.UTF8, "application/json");
-                var response = client.PostAsync(webhookLink, stringContent);
+                var response = await client.PostAsync(webhookLink, stringContent);
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return responseContent;
             }
-            catch (Exception ex) { Cli.Output.Error(ex.ToString()); }
+            catch (Exception ex) { Cli.Output.Error(ex.ToString()); return "null"; }
         }
         
-        public static void SendDiscordWebhooks(string sniped, bool prename=false) {
-            string json = prename 
+        public static async Task SendDiscordWebhooks(string sniped, bool prename=false) {
+            string jsonOfficial = prename 
             ? JsonSerializer.Serialize(new
             { 
                 name = sniped,
@@ -28,11 +30,45 @@ namespace Utils
                 sender = Config.v.DiscordWebhookUsername
             }, new JsonSerializerOptions { WriteIndented = true });
             if (Config.v.SnipesharpServerWebhook) 
-                Send(Config.v.snipesharpServerWebhook, json);
+                Send(Config.v.snipesharpServerWebhook, jsonOfficial);
             if (
                 !String.IsNullOrEmpty(Config.v.CustomDiscordWebhookUrl) &&
                 !String.IsNullOrEmpty(Config.v.DiscordWebhookUsername)
-            )   Send(Config.v.CustomDiscordWebhookUrl, json);
+            ){
+                var searchesResponse = await Send("http://webhooks.snipesharp.xyz/searches", JsonSerializer.Serialize(new {name=sniped}));
+                string json = JsonSerializer.Serialize(new {
+                    embeds = new[]
+                    {
+                        new
+                        {
+                            title = "snipesharp snipe :tada:",
+                            fields = new[] {
+                                new {
+                                    name = "Searches",
+                                    value = $"{searchesResponse} per month",
+                                    inline = true
+                                },
+                                new {
+                                    name = "Name sniped",
+                                    value = $"[{sniped}](https://namemc.com/{sniped})",
+                                    inline = true
+                                },
+                                new {
+                                    name = "Sniped by",
+                                    value = Config.v.DiscordWebhookUsername,
+                                    inline = true
+                                }
+                            },
+                            color = 1211647,
+                            thumbnail = new
+                            {
+                                url = $"https://mc-heads.net/head/{sniped}"
+                            }
+                        }
+                    }
+                }, new JsonSerializerOptions { WriteIndented = true});
+                Send(Config.v.CustomDiscordWebhookUrl, json);
+            }
         }
     }
 }
