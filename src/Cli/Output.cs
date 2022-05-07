86c167f00633499878e2fe3640387ee6eb2e49c7
dement6d <cuff.ho@gmail.com>
2022-05-07 02:42:24 +0200

    fix normal logo

--- src/Cli/Output.cs
+++ src/Cli/Output.cs
@@ -63,7 +63,7 @@ namespace Cli {
             Console.WriteLine(SetText.LightCyan +@"   _________  ______  ___  _____/ /_  ____ __________ ".Centered().Cross());
             Console.WriteLine(SetText.Cyan      +@"  / ___/ __ \/ / __ \/ _ \/ ___/ __ \/ __ `/ ___/ __ \".Centered().Cross());
             Console.WriteLine(SetText.DarkRed   +crosshair);
-            Console.WriteLine(SetText.Blue      +@"/____/_/ /_/ / .___/\___/____/_/ /_/\__,_/_/  / .___/ ".Centered().Cross());
+            Console.WriteLine(SetText.Blue      +@"/____/_/ /_/_/ .___/\___/____/_/ /_/\__,_/_/  / .___/ ".Centered().Cross());
             Console.WriteLine(SetText.DarkBlue  +@"            /_/                              /_/      ".Centered().Cross());
             Console.WriteLine($"{SetText.Blue}https://snipe{SetText.White}sharp.xyz{SetText.ResetAll}".Centered());
         }
