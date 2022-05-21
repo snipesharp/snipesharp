970182e4ff959293f11d0d46eb32ed04ae0d7cfa
dement6d <cuff.ho@gmail.com>
2022-05-21 15:51:00 +0200

    fix a bug in help page and improve help page

--- src/Cli/Output.cs
+++ src/Cli/Output.cs
@@ -79,16 +79,16 @@ namespace Cli {
                 $"Using arguments colored {SetText.Red}RED{SetText.ResetAll} could result in unwanted outcomes. Only use them if you know what you're doing!\n\n" +
                 "Available arguments:".Centered() +
                 "\n\n" +
-                "--" + SetText.Blue + "help".MakeGapRight(23) + SetText.ResetAll + $"Prints this page\n" +
+                "--" + SetText.Blue + "help".MakeGapRight(23) + SetText.ResetAll + $"Prints this page\n" + 
                 "--" + SetText.Blue + "install".MakeGapRight(23) + SetText.ResetAll + $"Installs snipesharp in {SetText.Blue}"
                     + (Cli.Core.pid == PlatformID.Unix
                     ? $"/usr/bin/snipesharp{SetText.ResetAll} ({SetText.Blue}Needs to be ran as superuser{SetText.ResetAll})\n"
                     : Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $"\\.snipesharp\\snipesharp.exe{SetText.ResetAll}\n") +
 
-                '\n' + $"{SetText.Blue}{SetText.Bold} CONFIG {SetText.ResetAll}".Centered(true, '-') +
+                "\n\n" + $"{SetText.Blue}{SetText.Bold} CONFIG {SetText.ResetAll}".Centered(true, '-') + "\n\n" +
                 "--" + SetText.Blue + "disable-discordrpc".MakeGapRight(23) + SetText.ResetAll + $"Disables Discord Rich Presence\n" +
                 "--" + SetText.Blue + "enable-discordrpc".MakeGapRight(23) + SetText.ResetAll + $"Enables Discord Rich Presence\n" +
-                "--" + SetText.Blue + "asc".MakeGapRight(23) + SetText.ResetAll + $"Enables Auto Skin Change\n" +
+                "--" + SetText.Blue + "asc".MakeGapRight(23) + SetText.ResetAll + $"Enables Auto Skin Change\n" + '\n' +
                 "--" + SetText.Blue + "asc-url".MakeGapRight(23) + SetText.ResetAll + $"Sets the Skin URL for Auto Skin Change ({SetText.Blue}STRING{SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "webhook-url".MakeGapRight(23) + SetText.ResetAll + $"Sets the Custom webhook URL ({SetText.Blue}STRING{SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "spread".MakeGapRight(23) + SetText.ResetAll + $"Sets the PacketSpreadMs config value ({SetText.Blue}INTEGER{SetText.ResetAll})\n" +
@@ -96,24 +96,24 @@ namespace Cli {
                 "--" + SetText.Blue + "offset".MakeGapRight(23) + SetText.ResetAll + $"Sets the offset in milliseconds. Use value 'auto' or 'suggested' to use the suggested value ({SetText.Blue}INTEGER{SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "username".MakeGapRight(23) + SetText.ResetAll + $"Sets your display name in Discord Rich Presence, if it's enabled ({SetText.Blue}STRING{SetText.ResetAll})\n" +
                 
-                '\n' + $"{SetText.Blue}{SetText.Bold} GETDROPPING API {SetText.ResetAll}".Centered(true, '-') +
+                "\n\n" + $"{SetText.Blue}{SetText.Bold} GETDROPPING API {SetText.ResetAll}".Centered(true, '-') + "\n\n" +
                 "--" + SetText.Blue + "api-minsearches".MakeGapRight(23) + SetText.ResetAll + $"Sets the minimum NameMC search count the name has to have ({SetText.Blue}INTEGER (RANGE: 0-10000){SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "api-length".MakeGapRight(23) + SetText.ResetAll + $"Sets the min/max/eq length value according to ApiLengthOption ({SetText.Blue}INTEGER (RANGE: 3-16){SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "api-lengthoption".MakeGapRight(23) + SetText.ResetAll + $"0 = none, 1 = Less or equal to ApiLength, 2 = Equal to ApiLength, 3 = Greater or equal to ApiLength ({SetText.Blue}INTEGER (RANGE: 0-3){SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "api-language".MakeGapRight(23) + SetText.ResetAll + $"Sets the language the name has to be in ({SetText.Blue}STRING{SetText.ResetAll})\n" +
 
-                '\n' + $"{SetText.Blue}{SetText.Bold} AUTHENTICATING / LOGIN {SetText.ResetAll}".Centered(true, '-') +
+                "\n\n" + $"{SetText.Blue}{SetText.Bold} AUTHENTICATING / LOGIN {SetText.ResetAll}".Centered(true, '-') + "\n\n" +
                 "--" + SetText.Blue + "bearer".MakeGapRight(23) + SetText.ResetAll + $"Sets the Bearer Token account value ({SetText.Blue}STRING{SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "email".MakeGapRight(23) + SetText.ResetAll + $"Sets the Microsoft login Email ({SetText.Blue}STRING{SetText.ResetAll}) ({SetText.Blue}Requires --password{SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "password".MakeGapRight(23) + SetText.ResetAll + $"Sets the Microsoft login Password ({SetText.Blue}STRING{SetText.ResetAll}) ({SetText.Blue}Requires --email{SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "mojang-email".MakeGapRight(23) + SetText.ResetAll + $"Sets the Mojang login Email ({SetText.Blue}STRING{SetText.ResetAll}) ({SetText.Blue}Requires --mojang-password{SetText.ResetAll})\n" +
                 "--" + SetText.Blue + "mojang-password".MakeGapRight(23) + SetText.ResetAll + $"Sets the Mojang login Password ({SetText.Blue}STRING{SetText.ResetAll}) ({SetText.Blue}Requires --mojang-email{SetText.ResetAll})\n" +
                 
-                '\n' + $"{SetText.Blue}{SetText.Bold} HELPERS {SetText.ResetAll}".Centered(true, '-') +
+                "\n\n" + $"{SetText.Blue}{SetText.Bold} HELPERS {SetText.ResetAll}".Centered(true, '-') + "\n\n" +
                 "--" + SetText.Blue + "prename".MakeGapRight(23) + SetText.ResetAll + $"Automatically sets SendPacketsCount to 2\n" +
                 "--" + SetText.Blue + "skip-gc-redeem".MakeGapRight(23) + SetText.ResetAll + $"Skips the giftcard redeeming process\n" +
                 
-                '\n' + $"{SetText.Blue}{SetText.Bold} EXPERIMENTAL {SetText.ResetAll}".Centered(true, '-') + 
+                "\n\n" + $"{SetText.Blue}{SetText.Bold} EXPERIMENTAL {SetText.ResetAll}".Centered(true, '-') + "\n\n" +
                 "--" + SetText.Blue + "periodically".MakeGapRight(23) + SetText.ResetAll + $"Snipes a name periodically, as often as defined in this argument ({SetText.Blue}INTEGER{SetText.ResetAll})\n" +
                 "--" + SetText.Red     + "test-rl".MakeGapRight(23) + SetText.ResetAll + $"Instantly sends name change packets for the name 'abc' to test rate limiting. Works well combined with --packet-count\n" +
                 "--" + SetText.Red     + "await-first-packet".MakeGapRight(23) + SetText.ResetAll + $"Sends the second name change packet after a response is received from the first one\n" +
